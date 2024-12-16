074ffafb8ff70c3315b0147a7d9666ba915c9063
Iran Garcia <irangarciaj@gmail.com>
2024-12-16 09:20:18 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Iran on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
